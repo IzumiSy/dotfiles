eb202cd3d760afc4d3a3b11bed34b1bad92e8bb0
IzumiSy <beetle-noise@gmx.com>
2016-07-23 14:17:50 +0900

    Add installers for rbenv and nvm

---
+++ nvm.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.3/install.sh | bash
+
+# TODO: Adds modification script here to load NVM.
+
---
+++ rbenv.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git clone https://github.com/rbenv/rbenv.git
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# TODO: Add modification script here to load rbenv.
+
