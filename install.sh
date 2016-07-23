0435d81fa64c0ee6ea6922bb283abe9917e03e1f
IzumiSy <beetle-noise@gmx.com>
2016-07-23 14:20:18 +0900

    Add vundle installation

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# To use vundle in Vim
+git clone https://github.com/gmarik/vundle.git ~/.vim/vundle.git
+
 ln -sf "$(pwd)/_vimrc" ~/_vimrc
 ln -sf "$(pwd)/_screenrc" ~/.screenrc
 ln -sf "$(pwd)/_bashrc" ~/.bashrc
