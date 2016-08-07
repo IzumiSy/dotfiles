4cae22f6668eca3d741f234addf43f74b44bd2c0
IzumiSy <beetle-noise@gmx.com>
2016-08-07 08:40:48 -0400

    Add directory check

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# To use vundle in Vim
-git clone https://github.com/gmarik/vundle.git ~/.vim/vundle.git
+# Clone Vundle to use vundle in Vim
+if [ ! -e "$HOME/.vim/vundle.git" ] ; then
+    git clone https://github.com/gmarik/vundle.git ~/.vim/vundle.git
+fi
 
 ln -sf "$(pwd)/_vimrc" ~/_vimrc
 ln -sf "$(pwd)/_screenrc" ~/.screenrc
