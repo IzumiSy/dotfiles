9bae8d6ff0fbc11a406105385e5ef81b7c49b6ef
IzumiSy <beetle-noise@gmx.com>
2016-09-23 20:33:15 +0900

    Revert

--- install.sh
+++ install.sh
@@ -7,15 +7,15 @@ WHITE='\033[1;37m'
 NC='\033[0m'
 
 print_for_log() {
-    echo -e "$1$2${NC}"
+    echo -e "${WHITE}$1${NC}"
 }
 
-print_for_log GREEN "Start installing dotfiles"
+print_for_log "Start installing dotfiles"
 
 #
 # Install _bashrc
 #
-print_for_log WHITE "Linking _bashrc..."
+print_for_log "Linking _bashrc..."
 
 ln -sf "$(pwd)/_bashrc" ~/.bashrc
 
@@ -26,24 +26,24 @@ fi
 #
 # Install _screenrc
 #
-print_for_log WHITE "Linking _screenrc"
+print_for_log "Linking _screenrc"
 
 ln -sf "$(pwd)/_screenrc" ~/.screenrc
 
 #
 # Install _vimrc
 #
-print_for_log WHITE "Linking _vimrc"
+print_for_log "Linking _vimrc"
 
 if [ ! -e "$HOME/.vim/bundle" ] ; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 ln -sf "$(pwd)/_vimrc" ~/_vimrc
 
-print_for_log WHITE "Install vim plugins"
+print_for_log "Install vim plugins"
 vim +PluginInstall +qall
 
-print_for_log WHITE "Build vimproc"
+print_for_log "Build vimproc"
 (cd ~/.vim/bundle/vimproc/; make)
 
-print_for_log GREEN "Finish installing dotfiles"
+print_for_log "Finish installing dotfiles"
