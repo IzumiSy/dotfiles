1c4d8f86bceef334a3237391ecd550e79cd607c1
IzumiSy <beetle-noise@gmx.com>
2016-10-14 13:05:02 +0900

    Add uninstall process for VSCode configs

--- uninstall.sh
+++ uninstall.sh
@@ -8,3 +8,12 @@ unlink ~/.bashrc
 unlink ~/.bash_profile
 
 rm -rf $HOME/.vim
+
+if [[ "$OSTYPE" = "darwin"* ]] ; then
+    vscode_config_path="$HOME/Library/Application Support/Code/User"
+else
+    vscode_config_path="$HOME/.config/Code/User/"
+fi
+
+unlink "${vscode_config_path}/settings.json"
+unlink "${vscode_config_path}/keybindings.json"
