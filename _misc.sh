f19044fa9b056627c59f27104b255e327657957c
IzumiSy <beetle-noise@gmx.com>
2016-10-30 23:54:27 +0900

    One liner

--- _misc.sh
+++ _misc.sh
@@ -4,10 +4,5 @@ GREEN='\033[0;32m'
 WHITE='\033[1;37m'
 NC='\033[0m'
 
-print_for_log() {
-    printf "${WHITE}$1${NC}\n"
-}
-
-print_for_log2() {
-    printf "${GREEN}$1${NC}\n"
-}
+print_for_log() { printf "${WHITE}$1${NC}\n"; }
+print_for_log2() { printf "${GREEN}$1${NC}\n"; }
