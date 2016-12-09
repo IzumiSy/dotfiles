1c27901df8425b79d9a883d09a6e9a6907a76902
IzumiSy <beetle-noise@gmx.com>
2016-12-09 16:17:47 +0900

    Modify selections

--- installers/nvm.sh
+++ installers/nvm.sh
@@ -5,10 +5,10 @@ function execute_nvm_install() {
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.3/install.sh | bash
 }
 
-echo "Looks nvm is not installed yet. Do you want to install it? [Y/n]"
+echo "Looks nvm is not installed yet. Do you want to install it? [yes/no]"
 read answer
 
 case $answer in
-  no) echo "Skipped nvm set-up."; ;;
-  yes|*) execute_nvm_install; ;;
+  yes) execute_nvm_install; ;;
+  no|*) echo "Skipped nvm set-up."; ;;
 esac
--- installers/rbenv.sh
+++ installers/rbenv.sh
@@ -6,10 +6,10 @@ function execute_rbenv_install() {
   git clone https://github.com/rbenv/ruby-build.git "$HOME/.rbenv/plugins/ruby-build"
 }
 
-echo "Looks rbenv is not installed yet. Do you want to install it? [Y/n]"
+echo "Looks rbenv is not installed yet. Do you want to install it? [yes/no]"
 read answer
 
 case $answer in
-  no) echo "Skipped nvm set-up."; ;;
-  yes|*) execute_rbenv_install; ;;
+  yes) execute_rbenv_install; ;;
+  no|*) echo "Skipped nvm set-up."; ;;
 esac
