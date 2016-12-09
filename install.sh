c96ee189f3cf2b7fee645bfb913ee388e70831d4
IzumiSy <beetle-noise@gmx.com>
2016-12-09 15:37:28 +0900

    Use home path

--- install.sh
+++ install.sh
@@ -4,30 +4,31 @@ set -Ceu
 #
 # Install _bashrc
 #
-ln -snvf "$(pwd)/_bashrc" ~/.bashrc
+ln -snvf "$(pwd)/_bashrc" "$HOME/.bashrc"
 
 if [ ! -e "$HOME/.bash_profile" ] ; then
-    ln -snvf "$(pwd)/_bash_profile" ~/.bash_profile
+    ln -snvf "$(pwd)/_bash_profile" "$HOME/.bash_profile"
 fi
 
 #
 # Install _screenrc
 #
-ln -snvf "$(pwd)/_screenrc" ~/.screenrc
+ln -snvf "$(pwd)/_screenrc" "$HOME/.screenrc"
 
 #
 # Install _vimrc
 #
 if [ ! -e "$HOME/.vim/bundle" ] ; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
 fi
-ln -snvf "$(pwd)/_vimrc" ~/_vimrc
+ln -snvf "$(pwd)/_vimrc" "$HOME/_vimrc"
 
 set -x
 vim +PluginInstall +qall
 (cd ~/.vim/bundle/vimproc/; make)
 
 # Loads .bashrc
+clear
 exec bash --login
 
 set +x
