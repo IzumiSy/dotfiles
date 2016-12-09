c71ea83307c91202cec835096ad8ba4b429503c3
IzumiSy <beetle-noise@gmx.com>
2016-12-09 15:34:37 +0900

    Set -Ceu

--- installers/nvm.sh
+++ installers/nvm.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-set -e
+set -Ceu
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.3/install.sh | bash
 
--- installers/rbenv.sh
+++ installers/rbenv.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-set -e
+set -Ceu
 
 git clone https://github.com/rbenv/rbenv.git "$HOME/.rbenv"
 git clone https://github.com/rbenv/ruby-build.git "$HOME/.rbenv/plugins/ruby-build"
