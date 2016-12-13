fcab32a396463f8ed4e27e7b54ebc21d8bfc1e3f
IzumiSy <beetle-noise@gmx.com>
2016-12-13 15:40:13 +0900

    Update _hyper.js

--- etc/_hyper.js
+++ etc/_hyper.js
@@ -38,7 +38,7 @@ module.exports = {
     showWindowControls: '',
 
     // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '0px 10px',
+    padding: '1px 10px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
@@ -91,7 +91,13 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    'hyperterm-colors',
+    'hyperterm-tabs',
+    'hyperterm-tab-icons',
+    'hyperterm-cursor',
+    'hyper-blink'
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
