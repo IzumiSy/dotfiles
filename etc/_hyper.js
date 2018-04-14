0bbcf461eb31ea85752436e2f77c9200c37adc2f
IzumiSy <beetle-noise@gmx.com>
2018-04-14 16:11:30 +0900

    Updated shellArgs .hyper.js

--- etc/_hyper.js
+++ etc/_hyper.js
@@ -68,7 +68,7 @@ module.exports = {
 
     // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
     // by default ['--login'] will be used
-    shellArgs: ['--login'],
+    shellArgs: ['-i'],
 
     // for environment variables
     env: {},
