eb66102b7c017f79cf4302001920735e6d7940ec
IzumiSy <beetle-noise@gmx.com>
2022-07-19 12:05:57 +0900

    Updated hyper configuration

--- etc/_hyper.js
+++ etc/_hyper.js
@@ -38,7 +38,7 @@ module.exports = {
     showWindowControls: 'left',
 
     // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '10px 10px 0 10px',
+    padding: '5px 10px 0 10px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
@@ -101,7 +101,7 @@ module.exports = {
   //   `@company/project`
   //   `project#1.0.1`
   plugins: [
-    "hyper-statusline",
+    // "hyper-statusline",
     // "hyper-gcp-status-line",
     "hyper-tabs-enhanced",
     "hyper-blink",
