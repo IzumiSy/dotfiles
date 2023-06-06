9bc2013692ffb8f7b1dd84445507a7e4e4ca06d2
Seiya IZUMI <IzumiSy@users.noreply.github.com>
2023-06-06 23:58:07 +0900

    Update install.ps1

--- vscode/install.ps1
+++ vscode/install.ps1
@@ -1,5 +1,10 @@
 # GithubからVSCodeの設定ファイルをダウンロードして配置する
 # 最新のmasterブランチのデータが欲しいのでno-cache指定にしておく
+# スクリプト実行の際には以下のコマンドで実行ポリシーを許可しておくこと
+#
+#  Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+#
+
 $VerbosePreference = 'Continue'
 wget -Headers @{"Cache-Control"="no-cache"} https://github.com/IzumiSy/dotfiles/raw/master/vscode/settings.json -O ~/AppData/Roaming/Code/User/settings.json
 wget -Headers @{"Cache-Control"="no-cache"} https://github.com/IzumiSy/dotfiles/raw/master/vscode/keybindings.json -O ~/AppData/Roaming/Code/User/keybindings.json
